cb46bbaf6fc7dd8284e16043b0f5cfdf53aea1f4
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-31 18:53:27 +1000

    Create wifimaker.py

---
+++ wifimaker.py
@@ -0,0 +1,22 @@
+import os
+
+
+class Wifimaker():
+self.wifiname = "SkyWifi"
+self.wifipassword = "Lhldy342DJ5SF4"
+
+
+def runwifi():
+  self(run->new(Internet()))
+
+def connecttowifi():
+  if devicedetails.connected is true:
+    print("a device has connected to skywifi: their ip address is", devicedetails.publipip)
+  else
+     print("a device has failed to connect to skywifi: their ip address is", devicedetails.publip) 
+
+def devicedetails():
+  self.privip = 192.168.1.1
+  self.publip = random(0.0.0.0, 200,200,200)
+  self.os = os.model
+  self.location = os.location
